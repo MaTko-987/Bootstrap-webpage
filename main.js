d1e4d8a1c9483496bf401c299af897ba8d352d53
Ma Tko <kwikzzstep@gmail.com>
2021-09-01 12:57:20 +0200

    5th commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+/* variables */
+const name = document.getElementById('name');
+const phoneNumber = document.getElementById('phoneNumber');
+const date = document.getElementById('date');
+
+
+
+/* name validation */
+
+
+
+/* phoneNumber validation */
+
+
+
+/* date validation */
+
