8f73bc519ef6da9cd2f29778ef3612643f06cf1b
Ma Tko <kwikzzstep@gmail.com>
2021-09-01 14:13:01 +0200

    6th commit

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const date = document.getElementById('date');
 
 /* name validation */
 
-
+if(name == ""){
+alert("You need to write your first and last name!")
+} else {
+alert("Thank you very much!")}
 
 /* phoneNumber validation */
 
