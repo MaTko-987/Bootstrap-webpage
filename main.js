08ce4a0e26c91586e91adce07ae5177477be964b
Ma Tko <kwikzzstep@gmail.com>
2021-09-03 12:11:52 +0200

    7th commit

--- main.js
+++ main.js
@@ -15,7 +15,16 @@ alert("Thank you very much!")}
 
 /* phoneNumber validation */
 
-
+if (phoneNUmber == ""){
+alert("You need to write your first and last name!")
+} else {
+alert("Thank you very much!")}
 
 /* date validation */
+
+if(date == ""){
+alert("You need to choose a date !")
+} else {
+alert ("Thank you very much!")
+}
 
